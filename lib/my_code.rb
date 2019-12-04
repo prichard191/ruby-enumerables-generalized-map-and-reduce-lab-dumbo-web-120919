53237ecccf15b62e7019b5259da349fec3417bf6
Patrick Richard <prichard191@gmail.com>
2019-12-04 18:29:46 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,25 @@
 # Your Code Here
+def map(bleep)
+  new = []
+  i = 0
+  while i < bleep.length
+    new.push(yield(bleep[i]))
+    i += 1
+  end
+  new
+end
+
+def reduce(s, spf=nil)
+  if spf
+    honk = spf
+    i = 0
+  else
+    honk = s[0]
+    i = 1
+  end
+  while i < s.length
+    honk = yield(honk, s[i])
+    i += 1
+  end
+  honk
+end
